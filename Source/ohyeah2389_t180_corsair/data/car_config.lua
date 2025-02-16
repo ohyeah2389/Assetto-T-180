bd4db598c1852eef51b6bd1ec5eb2811e16cc6b2
pl4sma2389 <ohyeah2389@yahoo.com>
2025-02-16 03:46:24 -0500

    v0.6.1pre1

---
+++ Source/ohyeah2389_t180_corsair/data/car_config.lua
@@ -0,0 +1,34 @@
+-- T-180 CSP Physics Script - Config and Parameters Module
+-- Authored by ohyeah2389
+
+
+local config = {
+    misc = {
+        brakeAutoHold = {
+            torque = 200; -- Brake torque in Nm to apply when auto-holding the brakes
+            speed = 10; -- Speed in kmh below which to auto-hold the brakes
+        };
+        debugFrequency = 0.1;
+        jumpJackSize = 1;
+    },
+    turbine = {
+        present = true, -- Whether the car has a turbine or not
+        frictionCoef = 1.35, -- Friction coefficient of the turbine shaft
+        inertia = 0.002, -- Inertia of the turbine shaft
+        minThrottle = 0.2, -- Idle throttle of the turbine
+        throttleLag = 0.9, -- 0 to 1 where 0 represents instant turbine throttle response (unrealistic) and 1 represents no change in throttle
+        throttleLagAfterburner = 0.8, -- 0 to 1 as above, but for afterburner fuel delivery
+        gearRatio = 5.0, -- value:1, turbine:engine
+        boostThrustFactor = 0.00001, -- Multiplier on turbine output thrust to form bleed air boost thrust component (speed component is added to this for total bleed air boost value)
+        boostSpeedFactor = 0.001, -- Multiplier on turbine angular speed to form bleed air boost speed component (thrust component is added to this for total bleed air boost value)
+        maximumEffectiveIntakeSpeed = 2000, -- kmh - Maximum effective intake speed of the turbine (used to calculate effective intake speed for thrust fadeout)
+        thrustFadeoutExponent = 1.2, -- Exponent for the thrust fadeout curve (used to calculate effective intake speed for thrust fadeout)
+        fuelTankCapacity = 100, -- Litres - Capacity of the fuel tank
+        fuelConsThrottleFactor = 1.0, -- Throttle position fuel consumption multiplier
+        fuelConsSpeedFactor = 0.0001, -- Angular speed fuel consumption multiplier
+        fuelConsThrustFactor = 0.000125, -- Developed thrust fuel consumption multiplier
+    }
+}
+
+
+return config
