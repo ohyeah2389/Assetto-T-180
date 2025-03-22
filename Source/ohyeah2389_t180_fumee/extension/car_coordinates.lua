107ab5a8aa6a592e05533db476810e7fb641b326
pl4sma2389 <ohyeah2389@yahoo.com>
2025-03-22 17:42:07 -0400

    Removed flame from Fumee (for replay)

--- Source/ohyeah2389_t180_fumee/extension/car_coordinates.lua
+++ Source/ohyeah2389_t180_fumee/extension/car_coordinates.lua
@@ -6,13 +6,13 @@ local config = {
         turbineExhaust = vec3(0.0, 0.494, -1.91)
     },
     flame = {
-        color = rgbm(1, 1, 1, 1),
-        afterburnerColor = rgbm(1, 1, 1, 1),
-        size = 3.2,
+        color = rgbm(0, 0, 0, 0),
+        afterburnerColor = rgbm(0, 0, 0, 0),
+        size = 0,
         temperatureMultiplier = 8,
         afterburnerTemperatureMultiplier = 10,
-        intensity = 0.9,
-        afterburnerIntensity = 1
+        intensity = 0,
+        afterburnerIntensity = 0
     }
 }
 
