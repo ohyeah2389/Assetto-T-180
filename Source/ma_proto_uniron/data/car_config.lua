82ad8c749288b8b6243db0864959797556595fa9
pl4sma2389 <ohyeah2389@yahoo.com>
2025-05-31 18:12:09 -0400

    Added Uniron Proto

---
+++ Source/ma_proto_uniron/data/car_config.lua
@@ -0,0 +1,43 @@
+-- T-180 CSP Physics Script - Config and Parameters Module
+-- Authored by ohyeah2389
+
+
+local config = {
+    misc = {
+        brakeAutoHold = {
+            torque = 200; -- Brake torque in Nm to apply when auto-holding the brakes
+            speed = 10; -- Speed in kmh below which to auto-hold the brakes
+        };
+        debugFrequency = 0.1;
+        jumpJackSize = 1;
+        traditionalSteering = true;
+    },
+    turbojet = {
+        present = false, -- Whether the car has a turbine or not
+        type = "single", -- "single" or "dual"
+        thrustApplicationPoint = vec3(0.0, 0.574, -2.323), -- Position of the thrust application point relative to the CoG
+        helperStartAngle = 20, -- Angle in degrees at which the turbojet starts to automatically apply throttle
+        helperEndAngle = 80, -- Angle in degrees at which the turbojet ends automatically applying throttle
+        frictionCoef = 0.08, -- Friction coefficient of the turbine shaft
+        inertia = 0.1, -- Inertia of the turbine shaft
+        minThrottle = 0.2, -- Idle throttle of the turbine
+        throttleLag = 0.6, -- 0 to 1 where 0 represents instant turbine throttle response (unrealistic) and 1 represents no change in throttle
+        throttleLagAfterburner = 0.6, -- 0 to 1 as above, but for afterburner fuel delivery
+        gearRatio = 2.5, -- value:1, turbine:engine
+        thrustMultiplier = 1.5, -- Multiplier on turbine output thrust
+        boostThrustFactor = 0.00001, -- Multiplier on turbine output thrust to form bleed air boost thrust component (speed component is added to this for total bleed air boost value)
+        boostSpeedFactor = 0.001, -- Multiplier on turbine angular speed to form bleed air boost speed component (thrust component is added to this for total bleed air boost value)
+        maximumEffectiveIntakeSpeed = 2000, -- kmh - Maximum effective intake speed of the turbine (used to calculate effective intake speed for thrust fadeout)
+        thrustFadeoutExponent = 1.2, -- Exponent for the thrust fadeout curve (used to calculate effective intake speed for thrust fadeout)
+        fuelTankCapacity = 100, -- Litres - Capacity of the fuel tank
+        fuelConsThrottleFactor = 1.0, -- Throttle position fuel consumption multiplier
+        fuelConsSpeedFactor = 0.0001, -- Angular speed fuel consumption multiplier
+        fuelConsThrustFactor = 0.000125, -- Developed thrust fuel consumption multiplier
+    },
+    turboshaft = {
+        present = false
+    }
+}
+
+
+return config
---
+++ Source/ma_proto_uniron/extension/car_coordinates.lua
@@ -0,0 +1,23 @@
+-- T-180 Effect Coordinates - Mach 6
+-- Authored by ohyeah2389
+
+local config = {
+    turbojetType = nil, -- "single" or "dual" or nil if not present
+    turboshaftPresent = false, -- true or false
+
+    coordinates = {
+        turbineExhaust = vec3(0.0, 0.574, -2.323)
+    },
+    flame = {
+        color = rgbm(1, 1, 1, 1),
+        afterburnerColor = rgbm(1, 1, 1, 1),
+        size = 3.2,
+        temperatureMultiplier = 1,
+        afterburnerTemperatureMultiplier = 2,
+        intensity = 0.9,
+        afterburnerIntensity = 1
+    },
+    turbineExhaustGlowMesh = "Mach6Turbine_SUB3"
+}
+
+return config
