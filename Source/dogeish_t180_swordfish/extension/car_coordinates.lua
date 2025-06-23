7590e2ef48a3fbb34adaa80e1b24b52138c7c6fc
pl4sma2389 <ohyeah2389@yahoo.com>
2025-06-22 20:49:08 -0400

    Added Swordfish, GRX

---
+++ Source/dogeish_t180_swordfish/extension/car_coordinates.lua
@@ -0,0 +1,22 @@
+-- T-180 Effect Coordinates - Talon
+-- Authored by ohyeah2389
+
+local config = {
+    turbojetType = "single", -- "single" or "dual" or nil if not present
+    turboshaftPresent = false, -- true or false
+
+    coordinates = {
+        turbineExhaust = vec3(0.0, 0.617, -2.1),
+    },
+    flame = {
+        color = rgbm(1, 0.8, 0.8, 1),
+        afterburnerColor = rgbm(1, 0.9, 0.9, 1),
+        size = 3.3,
+        temperatureMultiplier = 1.0,
+        afterburnerTemperatureMultiplier = 1.2,
+        intensity = 0.5,
+        afterburnerIntensity = 1.0
+    }
+}
+
+return config
---
+++ Source/ma_t180_grx/extension/car_coordinates.lua
@@ -0,0 +1,22 @@
+-- T-180 Effect Coordinates - Mach 6
+-- Authored by ohyeah2389
+
+local config = {
+    turbojetType = "single", -- "single" or "dual" or nil if not present
+    turboshaftPresent = false, -- true or false
+
+    coordinates = {
+        turbineExhaust = vec3(0.0, 0.38, -2.61)
+    },
+    flame = {
+        color = rgbm(1, 0.8, 0.8, 1),
+        afterburnerColor = rgbm(1, 1, 1, 1),
+        size = 3.2,
+        temperatureMultiplier = 1,
+        afterburnerTemperatureMultiplier = 2,
+        intensity = 0.9,
+        afterburnerIntensity = 1
+    }
+}
+
+return config
