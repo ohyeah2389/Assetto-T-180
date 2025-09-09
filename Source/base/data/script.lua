7e11d932869098aae5e8b2eba4209455f7cc0431
pl4sma2389 <ohyeah2389@yahoo.com>
2025-09-08 23:50:32 -0400

    Aero tuning

--- Source/base/data/script.lua
+++ Source/base/data/script.lua
@@ -306,7 +306,7 @@ function script.update(dt)
 
     local rideHeightSensor = physics.raycastTrack(car.position + (car.up * 0.4) + (car.look * 1.0), -car.up, 1.0)
     local suctionMult = math.clamp(math.remap(rideHeightSensor, 0.5, 0.9, 1, 0), 0, 1) * (rideHeightSensor == -1 and 0 or 1)
-    local aeroForce = -350 * (math.abs(car.localVelocity.x) + math.abs(car.localVelocity.z)) * suctionMult
+    local aeroForce = -325 * (math.abs(car.localVelocity.x) + math.abs(car.localVelocity.z)) * suctionMult
 
     ac.addForce(vec3(0, 0, 0), true, vec3(0, aeroForce, 0), true)
 
