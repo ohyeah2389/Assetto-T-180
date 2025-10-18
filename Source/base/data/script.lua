c5a781973bc997ea0395b1558a91fe8f928908ee
pl4sma2389 <ohyeah2389@yahoo.com>
2025-10-18 12:58:01 -0400

    Decreased downforce by 20%

--- Source/base/data/script.lua
+++ Source/base/data/script.lua
@@ -324,7 +324,7 @@ function script.update(dt)
 
     local rideHeightSensor = physics.raycastTrack(car.position + (car.up * 0.4) + (car.look * 1.0), -car.up, 1.0)
     local suctionMult = math.clamp(math.remap(rideHeightSensor, 0.5, 0.9, 1, 0), 0, 1) * (rideHeightSensor == -1 and 0 or 1)
-    local aeroForceBase = ((car.name == "ohyeah2389_proto_mach4") or (car.name == "ma_proto_uniron")) and -180 or -250
+    local aeroForceBase = ((car.name == "ohyeah2389_proto_mach4") or (car.name == "ma_proto_uniron")) and -160 or -200
     local aeroForce = aeroForceBase * (math.abs(car.localVelocity.x) + math.abs(car.localVelocity.z)) * suctionMult
 
     ac.addForce(vec3(0, 0, 0), true, vec3(0, aeroForce, 0), true)
