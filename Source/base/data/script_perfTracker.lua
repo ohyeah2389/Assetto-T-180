abef05573440c7323dc28dc138ce21ee0289c39f
pl4sma2389 <ohyeah2389@yahoo.com>
2025-10-20 01:29:34 -0400

    perfTracker data output to log with Extra C

--- Source/base/data/script_perfTracker.lua
+++ Source/base/data/script_perfTracker.lua
@@ -1,5 +1,9 @@
 -- T-180 CSP Physics Script - Performance Tracker Module
 -- Authored by ohyeah2389
+--
+-- This module tracks performance data during laps and outputs it to the log.
+-- Data is recorded at 2 Hz (every 0.5 seconds) during each lap.
+-- At lap completion, all data is printed to the log in CSV format.
 
 local state = require('script_state')
 local config = require('car_config')
@@ -10,15 +14,111 @@ local perfTracker = class("perfTracker")
 
 function perfTracker:initialize()
     self.perfData = {}
+    self.lapData = {}
+    self.recordTimer = 0
+    self.recordInterval = 0.5 -- 2 Hz = 0.5 second interval
+    self.currentLap = 0
+    self.lastLapCount = 0
 end
 
 function perfTracker:reset()
     self.perfData = {}
+    -- Don't reset lap data on reset, only on lap completion
+end
+
+function perfTracker:saveLapDataToCSV()
+    if #self.lapData == 0 then
+        return
+    end
+
+    -- Create CSV header
+    local header = "Time,Speed_KMH,Speed_MPH,ThrustForce_N,AfterburnerForce_N,WheelForce_N,TotalForce_N," ..
+        "PotentialAccel_MS2,PotentialAccel_G,WheelForce_FL,WheelForce_FR,WheelForce_RL,WheelForce_RR," ..
+        "Mass_KG,PosX,PosY,PosZ"
+
+    -- Build CSV content
+    local csvContent = header .. "\n"
+    for _, dataPoint in ipairs(self.lapData) do
+        csvContent = csvContent .. string.format(
+            "%.3f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.3f,%.3f,%.2f,%.2f,%.2f,%.2f,%.2f,%.3f,%.3f,%.3f\n",
+            dataPoint.time,
+            dataPoint.speedKmh,
+            dataPoint.speedMph,
+            dataPoint.thrustForce,
+            dataPoint.afterburnerForce,
+            dataPoint.wheelForce,
+            dataPoint.totalForce,
+            dataPoint.potentialAcceleration,
+            dataPoint.potentialAccelerationG,
+            dataPoint.wheelForceFrontLeft,
+            dataPoint.wheelForceFrontRight,
+            dataPoint.wheelForceRearLeft,
+            dataPoint.wheelForceRearRight,
+            dataPoint.mass,
+            dataPoint.posX,
+            dataPoint.posY,
+            dataPoint.posZ
+        )
+    end
+
+    -- Print CSV data to log
+    print(csvContent)
+end
+
+function perfTracker:recordDataPoint(dt)
+    local car = ac.getCar(0)
+
+    -- Record data point
+    local dataPoint = {
+        time = car.lapTimeMs / 1000.0,      -- Convert to seconds
+        speedKmh = car.speedKmh,
+        speedMph = car.speedKmh * 0.621371, -- Convert km/h to mph
+        thrustForce = self.perfData.thrustForce or 0,
+        afterburnerForce = self.perfData.afterburnerForce or 0,
+        wheelForce = self.perfData.wheelForce or 0,
+        totalForce = self.perfData.totalForce or 0,
+        potentialAcceleration = self.perfData.potentialAcceleration or 0,
+        potentialAccelerationG = (self.perfData.potentialAcceleration or 0) / 9.81,
+        wheelForceFrontLeft = self.perfData.wheelForces.frontLeft or 0,
+        wheelForceFrontRight = self.perfData.wheelForces.frontRight or 0,
+        wheelForceRearLeft = self.perfData.wheelForces.rearLeft or 0,
+        wheelForceRearRight = self.perfData.wheelForces.rearRight or 0,
+        mass = car.mass,
+        posX = car.position.x,
+        posY = car.position.y,
+        posZ = car.position.z
+    }
+
+    table.insert(self.lapData, dataPoint)
+end
+
+function perfTracker:checkLapCompletion()
+    local car = ac.getCar(0)
+    local currentLapCount = car.lapCount
+
+    -- Detect lap completion
+    if currentLapCount > self.lastLapCount and self.lastLapCount > 0 then
+        -- Lap completed, save data
+        self.currentLap = self.lastLapCount
+        self:saveLapDataToCSV()
+
+        -- Clear lap data for new lap
+        self.lapData = {}
+        self.recordTimer = 0
+    end
+
+    self.lastLapCount = currentLapCount
 end
 
 function perfTracker:update(dt)
     local car = ac.getCar(0)
     local carPhysics = ac.accessCarPhysics()
+
+    -- Check for lap completion
+    self:checkLapCompletion()
+
+    -- Update recording timer
+    self.recordTimer = self.recordTimer + dt
 
     -- Get wheel radius from car state
     local wheelRadius = car.wheels[0].tyreRadius
@@ -65,17 +165,16 @@ function perfTracker:update(dt)
             ac.debug("perfTracker_turbojet_right_afterburner", rightAfterburner)
             ac.debug("perfTracker_turbojet_total_afterburner", totalAfterburnerForce)
         end
-        
+
         -- For turbojet cars, get wheel forces directly from CPhys
         -- fx is "Force along car tyre" which should be the driving force
         wheelForces.frontLeft = carPhysics.wheels[0].fx * -1
         wheelForces.frontRight = carPhysics.wheels[1].fx * -1
         wheelForces.rearLeft = carPhysics.wheels[2].fx * -1
         wheelForces.rearRight = carPhysics.wheels[3].fx * -1
-        
-        totalWheelForce = wheelForces.frontLeft + wheelForces.frontRight + 
-                         wheelForces.rearLeft + wheelForces.rearRight
-        
+
+        totalWheelForce = wheelForces.frontLeft + wheelForces.frontRight + wheelForces.rearLeft + wheelForces.rearRight
+
         ac.debug("perfTracker_turbojet_wheel_force_FL", wheelForces.frontLeft)
         ac.debug("perfTracker_turbojet_wheel_force_FR", wheelForces.frontRight)
         ac.debug("perfTracker_turbojet_wheel_force_RL", wheelForces.rearLeft)
@@ -173,6 +272,17 @@ function perfTracker:update(dt)
         potentialAcceleration = potentialAcceleration,
         wheelForces = wheelForces
     }
+
+    -- Record data at 2 Hz if we're in a lap and recording is requested
+    if self.recordTimer >= self.recordInterval and car.lapCount > 0 and car.extraC then
+        self:recordDataPoint(dt)
+        self.recordTimer = 0
+        ac.debug("perfTracker_recording", "Recording data point #" .. #self.lapData)
+    end
+
+    -- Debug output for recording status
+    ac.debug("perfTracker_lap_count", car.lapCount)
+    ac.debug("perfTracker_data_points", #self.lapData)
 end
 
 return perfTracker
