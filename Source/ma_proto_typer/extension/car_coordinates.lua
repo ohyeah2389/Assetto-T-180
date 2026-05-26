26fc3828f450b3ef44e31dd3098363edc820739b
ohyeah2389 <ohyeah2389@yahoo.com>
2026-05-26 00:57:31 -0400

    Add initial Type R data

---
+++ Source/ma_proto_typer/extension/car_coordinates.lua
@@ -0,0 +1,19 @@
+-- T-180 Effect Coordinates - Uniron
+-- Authored by ohyeah2389
+
+local config = {
+    turbojetType = nil,
+    turboshaftPresent = false,
+    flame = {
+        color = rgbm(1, 1, 1, 1),
+        afterburnerColor = rgbm(1, 1, 1, 1),
+        size = 3.2,
+        temperatureMultiplier = 1,
+        afterburnerTemperatureMultiplier = 2,
+        intensity = 0.9,
+        afterburnerIntensity = 1
+    },
+    turbineDamageGlowMesh = "grille"
+}
+
+return config
