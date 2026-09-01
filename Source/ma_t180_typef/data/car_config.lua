bf6bfcd66daa8015476eed7af816637e7fdeac00
ohyeah2389 <ohyeah2389@yahoo.com>
2026-09-01 17:26:57 -0400

    Roughly equalize net power, default gearing

--- Source/ma_t180_typef/data/car_config.lua
+++ Source/ma_t180_typef/data/car_config.lua
@@ -21,7 +21,7 @@ local config = {
         boostThrustFactor = 0.0000075,                     -- Multiplier on turbine output thrust to form bleed air boost thrust component (speed component is added to this for total bleed air boost value)
         boostSpeedFactor = 0.001,                          -- Multiplier on turbine angular speed to form bleed air boost speed component (thrust component is added to this for total bleed air boost value)
         thrustCurveExponent = 0.75,                        -- Exponent for the thrust curve from 0 to Mach 1.0 (higher = more aggressive increase)
-        thrustCurveLevel = 0.2,                            -- Level of thrust increase/decrease with speed: >0 increases thrust, <0 decreases thrust, 0 is neutral
+        thrustCurveLevel = 0.35,                           -- Level of thrust increase/decrease with speed: >0 increases thrust, <0 decreases thrust, 0 is neutral
         supersonicDeratingFactor = 0.7,                    -- Multiplier for thrust above Mach 1.0 due to shock intake effects (0-1, where 1 = no derate)
     },
     turboshaft = {
